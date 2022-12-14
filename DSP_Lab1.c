4bc8562bfc940fdc195086d78584ecd4b6ae33ba
EmilianoMarcle <89201123+EmilianoMarcle@users.noreply.github.com>
2022-12-14 16:46:46 -0300

    Update DSP_Lab1.c

--- DSP_Lab1.c
+++ DSP_Lab1.c
@@ -1,9 +1,19 @@
 /*
  * Archivo:      DSP_Lab1.c
  * Descripción:  Procesamiento Digital de Señales - Laboratorio N°1
+ *
+ * Consigna: Realizar un programa aplicativo que sea capaz de digitalizar una señal
+ * a través del módulo del ADC disponible en la board FRDM-K64F a distintas velocidades
+ * de muestreo, las velocidades requeridas son 8K/S, 16K/S 22K/S, 44K/S y 48K/S.
+ * Los cambios de las velocidades de muestreo serán realizados con una de las teclas
+ * de la placa de evaluación, en forma de un buffer circular.
+ * Cada velocidad de muestreo se indicará a través de un color RGB del LED.
+ * Con otra tecla de la placa se habilitara la adquisición o se parara la misma (Run/Stop).
+ * Los valores adquiridos serán almacenados en memoria en un buffer circular de 512 muestras
+ *  del tipo q15 (fraccional 15bits) y a su vez serán enviados a través del DAC (de 12bits).
  */
 
-/*Include Files*/
+/* Include Files */
 #include <stdio.h>
 #include "board.h"
 #include "peripherals.h"
@@ -13,21 +23,27 @@
 #include "fsl_debug_console.h"
 #include "arm_math.h"
 
-
 /* Definitions and declarations */
-#define 	SR_8K   	7500
-#define 	SR_16K		3750
-#define 	SR_22K		2727
-#define 	SR_44K		1364
-#define 	SR_48K		1250
-#define 	buffer_size 512
+#define 	ticks_SR_8K  	7500U
+#define 	ticks_SR_16K    3750U
+#define 	ticks_SR_22K    2727U
+#define 	ticks_SR_44K    1364U
+#define 	ticks_SR_48K    1250U
+#define 	buffer_size     512U
+//#define     offset       (uint16_t)     32767
 
-volatile uint16_t ADC_Value;	// Variable que almacena conversion del ADC.
-uint16_t DAC_Value;				// Variable que almacena valor a transmitir por DAC.
-q15_t buffer[buffer_size];		// Buffer circular.
+/* Variable que almacena conversion del ADC. */
+volatile uint16_t adc_value;
+/* Variable que almacena conversion del ADC en formato fraccional */
+q15_t adc_value_frac;
+/* Buffer circular para almacenar 512 muestras */
+q15_t buffer[buffer_size];
+/* Variable que almacena valor a enviar por DAC */
+uint16_t dac_value;
 
 /* Function Prototypes */
-void cargaBuffer(q15_t buffer_data);
+void envioUART(uint16_t);
+void DAC_Test(void);
 
 /*
  * Main program.
@@ -40,6 +56,11 @@ int main(void) {
     BOARD_InitLEDsPins();
     BOARD_InitBootClocks();
     BOARD_InitBootPeripherals();
+
+    /* Inicialización en 8K/S - LED ROJO */
+    GPIO_PinWrite(BOARD_LED_RED_GPIO, BOARD_LED_RED_PIN, 0);
+    GPIO_PinWrite(BOARD_LED_BLUE_GPIO, BOARD_LED_BLUE_PIN, 1);
+    GPIO_PinWrite(BOARD_LED_GREEN_GPIO, BOARD_LED_GREEN_PIN, 1);
 
 #ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
     /* Init FSL debug console. */
@@ -63,47 +84,47 @@ void BOARD_SW2_IRQ_HANDLER(void){
 	uint32_t pin_flags = GPIO_PortGetInterruptFlags(GPIOC);
 
 	/* Verificación del pin que produjo la interrupción */
-	if(pin_flags & (1<<BOARD_SW2_GPIO_PIN)){
+	if(pin_flags & (1U << BOARD_SW2_GPIO_PIN)){
 		switch(sampling_Freq){
 		case 0: // 16K/S : BLUE LED
 			PIT_StopTimer(PIT_PERIPHERAL, PIT_CHANNEL_0);
-			PIT_SetTimerPeriod(PIT_PERIPHERAL, PIT_CHANNEL_0, SR_16K);
+			PIT_SetTimerPeriod(PIT_PERIPHERAL, PIT_CHANNEL_0, ticks_SR_16K);
 			PIT_StartTimer(PIT_PERIPHERAL, PIT_CHANNEL_0);
-			GPIO_PinWrite(BOARD_LED_RED_GPIO, BOARD_LED_RED_PIN, 0);
-			GPIO_PinWrite(BOARD_LED_BLUE_GPIO, BOARD_LED_BLUE_PIN, 1);
-			GPIO_PinWrite(BOARD_LED_GREEN_GPIO, BOARD_LED_GREEN_PIN, 0);
+			GPIO_PinWrite(BOARD_LED_RED_GPIO, BOARD_LED_RED_PIN, 1);
+			GPIO_PinWrite(BOARD_LED_BLUE_GPIO, BOARD_LED_BLUE_PIN, 0);
+			GPIO_PinWrite(BOARD_LED_GREEN_GPIO, BOARD_LED_GREEN_PIN, 1);
 			break;
 		case 1: // 22K/S : GREEN LED
 			PIT_StopTimer(PIT_PERIPHERAL, PIT_CHANNEL_0);
-			PIT_SetTimerPeriod(PIT_PERIPHERAL, PIT_CHANNEL_0, SR_22K);
-			PIT_StartTimer(PIT_PERIPHERAL, PIT_CHANNEL_0);
-			GPIO_PinWrite(BOARD_LED_RED_GPIO, BOARD_LED_RED_PIN, 0);
-			GPIO_PinWrite(BOARD_LED_BLUE_GPIO, BOARD_LED_BLUE_PIN, 0);
-			GPIO_PinWrite(BOARD_LED_GREEN_GPIO, BOARD_LED_GREEN_PIN, 1);
-			break;
-		case 2: // 44K/S : YELLOW LED
-			PIT_StopTimer(PIT_PERIPHERAL, PIT_CHANNEL_0);
-			PIT_SetTimerPeriod(PIT_PERIPHERAL, PIT_CHANNEL_0, SR_44K);
-			PIT_StartTimer(PIT_PERIPHERAL, PIT_CHANNEL_0);
-			GPIO_PinWrite(BOARD_LED_RED_GPIO, BOARD_LED_RED_PIN, 1);
-			GPIO_PinWrite(BOARD_LED_BLUE_GPIO, BOARD_LED_BLUE_PIN, 0);
-			GPIO_PinWrite(BOARD_LED_GREEN_GPIO, BOARD_LED_GREEN_PIN, 1);
-			break;
-		case 3: // 48K/S : VIOLET LED
-			PIT_StopTimer(PIT_PERIPHERAL, PIT_CHANNEL_0);
-			PIT_SetTimerPeriod(PIT_PERIPHERAL, PIT_CHANNEL_0, SR_48K);
+			PIT_SetTimerPeriod(PIT_PERIPHERAL, PIT_CHANNEL_0, ticks_SR_22K);
 			PIT_StartTimer(PIT_PERIPHERAL, PIT_CHANNEL_0);
 			GPIO_PinWrite(BOARD_LED_RED_GPIO, BOARD_LED_RED_PIN, 1);
 			GPIO_PinWrite(BOARD_LED_BLUE_GPIO, BOARD_LED_BLUE_PIN, 1);
 			GPIO_PinWrite(BOARD_LED_GREEN_GPIO, BOARD_LED_GREEN_PIN, 0);
 			break;
+		case 2: // 44K/S : YELLOW LED
+			PIT_StopTimer(PIT_PERIPHERAL, PIT_CHANNEL_0);
+			PIT_SetTimerPeriod(PIT_PERIPHERAL, PIT_CHANNEL_0, ticks_SR_44K);
+			PIT_StartTimer(PIT_PERIPHERAL, PIT_CHANNEL_0);
+			GPIO_PinWrite(BOARD_LED_RED_GPIO, BOARD_LED_RED_PIN, 0);
+			GPIO_PinWrite(BOARD_LED_BLUE_GPIO, BOARD_LED_BLUE_PIN, 1);
+			GPIO_PinWrite(BOARD_LED_GREEN_GPIO, BOARD_LED_GREEN_PIN, 0);
+			break;
+		case 3: // 48K/S : VIOLET LED
+			PIT_StopTimer(PIT_PERIPHERAL, PIT_CHANNEL_0);
+			PIT_SetTimerPeriod(PIT_PERIPHERAL, PIT_CHANNEL_0, ticks_SR_48K);
+			PIT_StartTimer(PIT_PERIPHERAL, PIT_CHANNEL_0);
+			GPIO_PinWrite(BOARD_LED_RED_GPIO, BOARD_LED_RED_PIN, 0);
+			GPIO_PinWrite(BOARD_LED_BLUE_GPIO, BOARD_LED_BLUE_PIN, 0);
+			GPIO_PinWrite(BOARD_LED_GREEN_GPIO, BOARD_LED_GREEN_PIN, 1);
+			break;
 		case 4:	// 8K/S : RED LED
 			PIT_StopTimer(PIT_PERIPHERAL, PIT_CHANNEL_0);
-			PIT_SetTimerPeriod(PIT_PERIPHERAL, PIT_CHANNEL_0, SR_8K);
+			PIT_SetTimerPeriod(PIT_PERIPHERAL, PIT_CHANNEL_0, ticks_SR_8K);
 			PIT_StartTimer(PIT_PERIPHERAL, PIT_CHANNEL_0);
-			GPIO_PinWrite(BOARD_LED_RED_GPIO, BOARD_LED_RED_PIN, 1);
-			GPIO_PinWrite(BOARD_LED_BLUE_GPIO, BOARD_LED_BLUE_PIN, 0);
-			GPIO_PinWrite(BOARD_LED_GREEN_GPIO, BOARD_LED_GREEN_PIN, 0);
+			GPIO_PinWrite(BOARD_LED_RED_GPIO, BOARD_LED_RED_PIN, 0);
+			GPIO_PinWrite(BOARD_LED_BLUE_GPIO, BOARD_LED_BLUE_PIN, 1);
+			GPIO_PinWrite(BOARD_LED_GREEN_GPIO, BOARD_LED_GREEN_PIN, 1);
 		default:
 			break;
 		}
@@ -115,7 +136,7 @@ void BOARD_SW2_IRQ_HANDLER(void){
 	}
 
 	/* Limpia bandera de interrupción en SW2 */
-	GPIO_PortClearInterruptFlags(GPIOC, (1<<BOARD_SW2_GPIO_PIN));
+	GPIO_PortClearInterruptFlags(GPIOC, (1U << BOARD_SW2_GPIO_PIN));
 
 	/* Add for ARM errata 838869, affects Cortex-M4, Cortex-M4F
 	Store immediate overlapping exception return operation might vector to incorrect interrupt. */
@@ -138,7 +159,7 @@ void BOARD_SW3_IRQ_HANDLER(void){
 	uint32_t pin_flags = GPIO_PortGetInterruptFlags(GPIOA);
 
 	/* Verificación del pin que produjo la interrupción */
-	if(pin_flags & (1<<BOARD_SW3_GPIO_PIN)){
+	if(pin_flags & (1U << BOARD_SW3_GPIO_PIN)){
 		if(startFlag == 0){
 			PIT_StartTimer(PIT_PERIPHERAL, PIT_CHANNEL_0);	// Run
 			EnableIRQ(PIT_CHANNEL_0_IRQN);
@@ -151,7 +172,7 @@ void BOARD_SW3_IRQ_HANDLER(void){
 		}
 	}
 	/* Limpia bandera de interrupción en SW3 */
-	GPIO_PortClearInterruptFlags(GPIOA, (1<<BOARD_SW3_GPIO_PIN));
+	GPIO_PortClearInterruptFlags(GPIOA, (1U << BOARD_SW3_GPIO_PIN));
 
 	/* Add for ARM errata 838869, affects Cortex-M4, Cortex-M4F
 	Store immediate overlapping exception return operation might vector to incorrect interrupt. */
@@ -189,45 +210,56 @@ void PIT_CHANNEL_0_IRQHANDLER(void)
  * 	ADC0 Interrupt Handler
  */
 void ADC0_IRQHANDLER(void) {
-	/* Variable temporal que almacena resultado de la conversion del ADC en formato q15 */
-	q15_t ADC_Value_fix;
-	/* Inicialización variable que almacena resultado de la conversión del ADC */
-	ADC_Value = 0;
+	/* Inicializacion variable que almacena la conversión del ADC */
+	adc_value = 0;
+	/* Indice que indica la posición del arreglo (dirección del buffer) */
+	static uint16_t index_buffer = 0;
+
 	/* Lectura de bandera Done */
 	uint16_t status = ADC16_GetChannelStatusFlags(ADC0_PERIPHERAL, ADC0_CH0_CONTROL_GROUP);
 	if ( status == kADC16_ChannelConversionDoneFlag){
 		/* Lectura del valor convertido por el ADC */
-		ADC_Value = (ADC16_GetChannelConversionValue(ADC0_PERIPHERAL, ADC0_CH0_CONTROL_GROUP) & (0xFFFF));
+		adc_value = (ADC16_GetChannelConversionValue(ADC0_PERIPHERAL, ADC0_CH0_CONTROL_GROUP)) & 0xFFFF;
+		/* Transformación del valor convertido por el ADC de 16 bits en q15_t */
+		adc_value_frac =  (q15_t) adc_value;
+		/* Almacena dato en buffer circular de 512 muestras */
+		buffer[index_buffer] = adc_value_frac;
+		index_buffer++;
+		if(index_buffer == buffer_size){
+			index_buffer = 0;
+		}
+		/* Transformación del valor de 16 bits para transmitirlo por DAC de 12 bits.
+		 * Se descartan los 4 LSB */
+	        dac_value = (adc_value >> 4) & 0x0FFF ;
+		/* Envía datos al DAC */
+		DAC_SetBufferValue(DAC0_PERIPHERAL, 0U, dac_value);
+		/* --- Serial Oscilloscope --- */
+		PRINTF("%d\r\n", adc_value);
+
+		/* --------- Lab View ---------*/
+		/* Envia por UART valor adquirido por ADC */
+		envioUART(adc_value);
 	}
-	/* Transformación del valor convertido */
-	ADC_Value_fix =  (q15_t) ADC_Value;
-	/* Almacena dato en buffer circular en formato q15*/
-	cargaBuffer(ADC_Value_fix);
-	/* Transformacion del valor para transmitirlo por DAC de 12 bits*/
-	DAC_Value = ((ADC_Value >> 4) & (0xFFF)); // Se descartan los 4 bits menos significativos.
-  	/* Envia datos al DAC */
-	DAC_SetBufferValue(DAC0_PERIPHERAL, 0U, DAC_Value);
 
   	/* Add for ARM errata 838869, affects Cortex-M4, Cortex-M4F
      Store immediate overlapping exception return operation might vector to incorrect interrupt. */
   	#if defined __CORTEX_M && (__CORTEX_M == 4U)
     	__DSB();
     #endif
+
+    return;
 }
 
 /*
- * Función que implementa el buffer circular.
+ *  Función que transmite el dato adquirido por UART.
  */
-void cargaBuffer(q15_t buffer_data){
-	/* Indice que indica la posición del arreglo (direccion del buffer) */
-	static uint16_t index_buffer = 0;
-
-	buffer[index_buffer] = buffer_data;
-	  	index_buffer++;
-	  	if(index_buffer == buffer_size){
-	  		index_buffer = 0;
-	  	}
+void envioUART(uint16_t ADC_Value){
+	uint8_t data = 0;
+	// Transmite parte alta del byte
+	data = (uint8_t) ((ADC_Value >> 8) & (0x00FF));
+	UART_WriteByte(UART0_PERIPHERAL, data);
+	// Transmite parte baja del byte
+	data = (uint8_t) (ADC_Value & (0x00FF));
+	UART_WriteByte(UART0_PERIPHERAL, data);
 	return;
 }
-
-
